ec32ae196d1367225d576ca1837f3655adf210d2
Hunter00000000 <131566518+Hunter00000000@users.noreply.github.com>
2023-04-22 21:10:08 -0500

    Update and rename give resouce to give resouce.js

---
+++ give resouce.js
@@ -0,0 +1,11 @@
+G.AddData({
+	name:'Theism',
+	author:'Owen "Ohead" Parker',
+	desc:'Adds Polytheism and Monotheism, both with different perks',
+	engineVersion:1,
+	requires:['Default dataset*'],
+	func:function()
+	{
+    G.fastTicks = 100
+  }
+});
